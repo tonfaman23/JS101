a735ecf88ce1db2e44e2e25fb97eb1cd056724f6
Will Baker <1will.baker@gmail.com>
2020-01-20 21:05:06 -0600

    added arrays and functions

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 console.log("Starting on arrays");
+
+//.push to add at end of array
+
+const nums = [1,2,3,4,5];
+const creatures = ['cat', 'dog', 'bear', 'turtle', 'fish'];
+const valuePrinter = () => {
+return nums[2];
+};
+console.log(valuePrinter(nums));
+//console.log(valuePrinter(creatures));
