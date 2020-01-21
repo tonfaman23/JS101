6353c472f30265305bfa67200cf09d70873f496c
Will Baker <1will.baker@gmail.com>
2020-01-20 20:43:46 -0600

    added objects and their functions

--- objects.js
+++ objects.js
@@ -17,3 +17,40 @@ const ageCheck = (age) => {
 
 ageCheck('12');
 ageCheck('22');
+
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'San Jose',
+    cost: 324.99,
+    isGood: true
+};
+
+//dot notation
+console.log(expense.cost);
+
+//bracket notation
+console.log(expense['cost']);
+
+const employee = {
+    id: 'RJ2468',
+    dateHired: '01/22/2017',
+    position: 'Account Manager',
+    salary: 75000,
+    firstName: 'Reynald',
+    lastName: 'Jemane',
+    passScreen: true,
+    collegeDegree: true,
+    supervisor: 'HK0064'
+};
+
+const statusMaker = (employee) => {
+    if(employee.firstName.charAt(0) === 'M'){
+        employee.status = 'VIP';
+    }
+    else{
+        employee.status = 'Peasant';
+    }
+    return employee;
+}
+
+console.log(statusMaker(employee));
